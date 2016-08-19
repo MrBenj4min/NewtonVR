6fb4e12d7dd04e594e0920ec5abc50ce4fd9edc5
Kristoffer Benjaminsson <kristoffer.benjaminsson@paranoidcorp.com>
2016-08-19 10:49:26 +0200

    Prevent SetDeviceIndex from being called more than once per hand

--- Assets/NewtonVR/OVR/NVRHandOculus.cs
+++ Assets/NewtonVR/OVR/NVRHandOculus.cs
@@ -13,14 +13,21 @@ public class NVRHandOculus : MonoBehaviour
 
     Transform trackedTransform = null;
 
+    static bool[] initialized = new bool[2];
+
     void Start ()
     {
+        int handIdx = (int)handedness;
+
         OVRCameraRig cameraRig = GameObject.FindObjectOfType<OVRCameraRig>();
         if (cameraRig != null)
         {
             trackedTransform = (handedness == HandednessId.Left) ? cameraRig.leftHandAnchor : cameraRig.rightHandAnchor;
-            BroadcastMessage("SetDeviceIndex", (int)handedness, SendMessageOptions.DontRequireReceiver); //Simulate SteamVR behaviour
+            if (NVRHandOculus.initialized[handIdx] == false) // prevent this message from being sent more than one per hand
+                BroadcastMessage("SetDeviceIndex", (int)handedness, SendMessageOptions.DontRequireReceiver); //Simulate SteamVR behaviour
         }
+
+        NVRHandOculus.initialized[handIdx] = true;
     }
 
     void LateUpdate ()
